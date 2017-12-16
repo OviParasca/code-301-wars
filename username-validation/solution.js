4f042849475b68388f5732044db93a20e85edff9
Ovidiu Parasca <OParasca@ApolloRoboticsTechnology.com>
2017-12-16 13:22:12 -0800

    - added the Alternating Caps code and files

--- username-validation/solution.js
+++ username-validation/solution.js
@@ -1,4 +1,15 @@
-function abbrevName(name) {
-  var matches = name.match(/\b(\w)/g); 
-  return matches.join('.').toUpperCase();
-}
+function capitalize(s) {
+  let finalArr = [];
+  for (i = 0; i < 2; i++) {
+    let tempStr = s;
+    for (j = 0; j < s.length; j++) {
+     if (i ==0 && j%2 == 0) {
+         tempStr = tempStr.substr(0, j) + tempStr[j].toUpperCase() + tempStr.substr(j + 1);
+     } else if (i == 1 && j%2 != 0) {
+         tempStr = tempStr.substr(0, j) + tempStr[j].toUpperCase() + tempStr.substr(j + 1);
+     }
+    }
+    finalArr.push(tempStr);
+  }  
+  return finalArr;
+};
